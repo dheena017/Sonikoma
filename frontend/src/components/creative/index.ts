b2cfcd8955b0a6588a8e1371c8ddf174cd97dcac
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-07-08 12:37:06 +0530

    feat: export creative suite components from index barrel file

---
+++ frontend/src/components/creative/index.ts
@@ -0,0 +1,5 @@
+export { default as CreativeSuiteLayout } from "./CreativeSuiteLayout";
+export { default as CreativeSuiteHeader } from "./CreativeSuiteHeader";
+export { default as CreativeSuiteSidebar } from "./CreativeSuiteSidebar";
+export { default as CreativeSuiteMiniSidebar } from "./CreativeSuiteMiniSidebar";
+export { default as CreativeSuiteDashboardPage } from "./CreativeSuiteDashboardPage";
