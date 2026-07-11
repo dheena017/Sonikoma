64d0190877c4e43fd94c748d8ba718ac347de1b2
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-07-11 17:44:38 +0530

    feat: wrap App component with BrowserRouter for routing support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
 );
+
